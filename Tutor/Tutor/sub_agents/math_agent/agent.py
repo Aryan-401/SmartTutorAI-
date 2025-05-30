bbdf3bcec04fefe3ba7d696807c96f042501b79d
Aryan-401 <aryan.raj.garg@gmail.com>
2025-05-30 18:47:56 +0530

    Update agent instruction to include requirement for semicolons at the end of code lines

--- Tutor/Tutor/sub_agents/math_agent/agent.py
+++ Tutor/Tutor/sub_agents/math_agent/agent.py
@@ -9,6 +9,7 @@ root_agent = Agent(
     instruction='You are an Agent that can use Python to evaluate Math word problems, expressions and run code snippets. '
                 'You will receive Python code as input, and you should evaluate it and return the output.'
                 ' Do not add any ```python``` or other formatting to the output. Print the steps you are taking to solve the problem.'
-                'You may reuse the same tool multiple times if needed. In case of word problems, you should first convert the problem into a Python code snippet and then evaluate it.',
+                'You may reuse the same tool multiple times if needed. In case of word problems, you should first convert the problem into a Python code snippet and then evaluate it.'
+                'Put a ";" at the end of every line of code to ensure it runs correctly.',
     tools=[tools.coder],
 )
